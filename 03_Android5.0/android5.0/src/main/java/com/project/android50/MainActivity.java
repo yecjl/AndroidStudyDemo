398114207dd8ba0cbc440b1aa77455c291298546
yecjl <773512439@qq.com>
2017-05-04 23:01:58 +0800

    commit andorid 5.0

---
+++ 03_Android5.0/android5.0/src/main/java/com/project/android50/MainActivity.java
@@ -0,0 +1,14 @@
+package com.project.android50;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO 需要优化
+    }
+}
